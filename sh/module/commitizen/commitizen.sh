3fcd8f451923ad52cc9128b54d120ed0cc60ee18
username4gh <jhon.doe1051@gmail.com>
2016-03-14 23:58:51 +0800

    feat: add commitizen

---
+++ sh/module/commitizen/commitizen.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+_commitizen_fix_package_json() {
+    if [[ "$#" == 0 ]];then
+        if [[ ! -d "$MY_REPO/cz-cli" ]];then
+            git clone https://github.com/commitizen/cz-cli "$MY_REPO/cz-cli"
+        fi
+        cp "$MY_REPO/cz-cli/package.json" ./
+    fi
+}
+
+_commitizen_init() {
+    commitizen init cz-conventional-changelog --save-dev --save-exact --force
+}
