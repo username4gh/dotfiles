5c481fdd7014d785931c7248e74fcc6be7d08e3d
username4gh <jhon.doe1051@gmail.com>
2016-04-13 10:51:04 +0800

    feat: rvm_completion add alfi

--- sh/module/rvm/src/rvm_completion.sh
+++ sh/module/rvm/src/rvm_completion.sh
@@ -2,7 +2,7 @@
 
 _virtual_rvm_complete () {
     local cur="${COMP_WORDS[COMP_CWORD]}"
-    local enable_disable_args="rvm"
+    local enable_disable_args="alfi rvm"
     COMPREPLY=( $(compgen -W "${enable_disable_args}" -- ${cur}) )
 }
 
