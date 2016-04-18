d03810aa0ef1d691c5ef60facdbf86b38588c1fe
username4gh <jhon.doe1051@gmail.com>
2016-04-18 21:12:46 +0800

    feat: changeshell.sh

--- sh/mechanism/autodetect.sh
+++ sh/mechanism/autodetect.sh
@@ -2,7 +2,7 @@
 
 _autodetect_bin() {
     if [[ "$#" == 1 ]];then
-        // it seems like that zsh treat 'path' same as 'PATH', so here we use bin_path
+        # it seems like that zsh treat 'path' same as 'PATH', so here we use bin_path
         local bin_path
         bin_path=$(ls -F $MY_BIN | s "$1" | tail -1)
         echo $MY_BIN/$bin_path
