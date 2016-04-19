638211624e987237dcfc6a6e680571645755773b
username4gh <jhon.doe1051@gmail.com>
2016-04-19 17:45:13 +0800

    feat: add 'gem' to virtual_rvm completion

--- sh/module/rvm/src/rvm_completion.sh
+++ sh/module/rvm/src/rvm_completion.sh
@@ -3,7 +3,7 @@
 if [[ "$MY_CURRENT_SHELL" == 'bash' ]];then
     _virtual_rvm_complete () {
         local cur="${COMP_WORDS[COMP_CWORD]}"
-        local enable_disable_args="alfi rvm"
+        local enable_disable_args="alfi gem rvm"
         COMPREPLY=( $(compgen -W "${enable_disable_args}" -- ${cur}) )
     }
 
