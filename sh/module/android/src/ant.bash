b06ab6ce2c3c20e0af48f166d991e757ce01ebd2
username4gh <jhon.doe1051@gmail.com>
2016-07-13 21:36:41 +0800

    fix: ant & mvn

--- sh/module/android/src/ant.bash
+++ sh/module/android/src/ant.bash
@@ -2,9 +2,9 @@
 
 _android_ant_latest_url() {
     local latest_version_code
-    latest_version_code=$(curl -s http://ant.apache.org/bindownload.cgi| s -o '(?<=Apache\ Ant\ ).*?(?=\ is\ the\ best\ available\ version)')
+    latest_version_code=$(curl -s https://ant.apache.org/bindownload.cgi| s -o '(?<=Apache\ Ant\ ).*?(?=\ is\ the\ best\ available\ version)')
     _conf_write 'ANT_LATEST_VERSION' $latest_version_code
-    echo "http://mirrors.noc.im/apache//ant/binaries/apache-ant-$latest_version_code-bin.tar.gz"
+    echo "https://www.apache.org/dist/ant/binaries/apache-ant-$latest_version_code-bin.tar.gz"
 }
 
 export PATH="$(_autodetect_bin 'apache-ant-.*')/bin:$PATH"
