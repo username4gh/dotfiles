f6e8377cde0d07ed815e2d2352cfbad7cdef40cf
username4gh <jhon.doe1051@gmail.com>
2016-10-05 16:11:46 +0800

    feat: completion for repo

---
+++ sh/module/repo/src/repo.bash
@@ -0,0 +1,30 @@
+ _completion_register_write repo abandon       
+_completion_register_write repo branch        
+_completion_register_write repo branches      
+_completion_register_write repo checkout      
+_completion_register_write repo cherry-pick   
+_completion_register_write repo diff          
+_completion_register_write repo diffmanifests 
+_completion_register_write repo download      
+_completion_register_write repo forall        
+_completion_register_write repo gitc-delete   
+_completion_register_write repo gitc-init     
+_completion_register_write repo grep          
+_completion_register_write repo help          
+_completion_register_write repo info         
+_completion_register_write repo init          
+_completion_register_write repo list          
+_completion_register_write repo manifest      
+_completion_register_write repo overview      
+_completion_register_write repo prune         
+_completion_register_write repo rebase        
+_completion_register_write repo selfupdate    
+_completion_register_write repo smartsync     
+_completion_register_write repo stage          
+_completion_register_write repo start          
+_completion_register_write repo status         
+_completion_register_write repo sync           
+_completion_register_write repo upload         
+_completion_register_write repo version
+
+_completion_setup repo
