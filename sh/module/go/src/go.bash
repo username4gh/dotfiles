6555a2264268c9e1a9f9ebbbc89a4a9df7058f65
username4gh <jhon.doe1051@gmail.com>
2016-10-12 10:46:32 +0800

    feat: add some condition clause to it, leave some room for flexibility

--- sh/module/go/src/go.bash
+++ sh/module/go/src/go.bash
@@ -1,6 +1,11 @@
 #! /usr/bin/env bash
-export GOROOT="$MY_BIN/go"
-export PATH="$GOROOT/bin:$PATH"
 
-export GOPATH="$HOME/go"
-export PATH="$GOPATH/bin:$PATH"
+if [[ -d "$MY_BIN/go" ]];then
+    export GOROOT="$MY_BIN/go"
+    export PATH="$GOROOT/bin:$PATH"
+fi
+
+if [[ -d "$HOME/go" ]];then
+    export GOPATH="$HOME/go"
+    export PATH="$GOPATH/bin:$PATH"
+fi
