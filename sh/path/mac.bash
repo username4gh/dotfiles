6a934ae8a21d6f35a72d54e8cd1ab667bb748cc6
username4gh <jhon.doe1051@gmail.com>
2016-10-27 15:01:53 +0800

    feat: sh/path/mac.bash support homebrew

--- sh/path/mac.bash
+++ sh/path/mac.bash
@@ -1,16 +1,22 @@
 #! /usr/bin/env bash
 if [[ "$(_check_os)" == 'Darwin' ]]; then
-    # no need to install any '*completion' package, leave it to oh-my-zsh/bash-it
+    # using macports
+    if [[ -f "/opt/local/bin/port" ]];then
+        # no need to install any '*completion' package, leave it to oh-my-zsh/bash-it
 
-    # keep macports remaining completely isolated
-    export PATH="/opt/local/bin:$PATH"
-    export PATH="/opt/local/sbin:$PATH"
-    export PATH="/opt/local/libexec/gnubin:$PATH"
+        # keep macports remaining completely isolated
+        export PATH="/opt/local/bin:$PATH"
+        export PATH="/opt/local/sbin:$PATH"
+        export PATH="/opt/local/libexec/gnubin:$PATH"
 
-    export MANPATH="/usr/share/man:/usr/local/share/man:/usr/X11/man"
-    export MANPATH="/opt/local/share/man:/opt/local/man:${MANPATH}"
+        export MANPATH="/usr/share/man:/usr/local/share/man:/usr/X11/man"
+        export MANPATH="/opt/local/share/man:/opt/local/man:${MANPATH}"
 
-    if [ -f /opt/local/etc/profile.d/bash_completion.sh ]; then
-        . /opt/local/etc/profile.d/bash_completion.sh
+        if [ -f /opt/local/etc/profile.d/bash_completion.sh ]; then
+            . /opt/local/etc/profile.d/bash_completion.sh
+        fi
+    else
+        # using homebrew
+        export PATH="/usr/local/bin:$PATH"
     fi
 fi
