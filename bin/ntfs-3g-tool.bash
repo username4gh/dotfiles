6e13dc8d6c0d222293c08630b5afc1d447d26955
username4gh <jhon.doe1051@gmail.com>
2016-11-01 11:15:09 +0800

    feat: ntfs-3g-tool.bash

---
+++ bin/ntfs-3g-tool.bash
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    if [[ "$#" -eq 2 ]];then
+        device=/dev/disk$(diskutil list | s -i $1 | s -o '(?<=disk).*?(?=$)')
+        echo $device
+
+        if [[ ! -e "$device" ]];then
+            echo "cannot find such device"
+            exit
+        else
+            echo "1. umount $device"
+            sudo umount $device
+        fi
+
+        if ! command -v "ntfs-3g" > /dev/null; then
+            echo "plz install ntfs-3g first"
+            exit
+        else
+            echo "ntfs-3g $device $2"
+            sudo ntfs-3g $device $2
+        fi
+    else
+        echo "Usage: command device_qualifier mount_point"
+        echo "For device_qualifier, use `diskutil list` to check candidates"
+        echo "Example: command QPTP /mnt"
+    fi
+else 
+    echo "Only works for Darwin"
+fi
