09d291234f9443b5778fe45f8818ab42ddb89887
username4gh <jhon.doe1051@gmail.com>
2016-11-15 00:08:58 +0800

    fix: crutils_index.bash

--- sh/module/code_reading_utils/script/crutils_index.bash
+++ sh/module/code_reading_utils/script/crutils_index.bash
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
 echo "cscope indexing ..."
-find $PWD -name '*.aidl' -o -name '*.cc' -o -name '*.h' -o -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.py' > './cscope.files'
+find -L $PWD -name '*.aidl' -o -name '*.cc' -o -name '*.h' -o -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.py' > './cscope.files'
 cscope -bckq -i './cscope.files'
 
 echo "ctags indexing ..."
 ctags -R --links=no --tag-relative=yes --exclude=.svn --exclude=.git -f './tags'
 
 echo "gnu global indexing ..."
-gtags
+gtags --skip-unreadable
