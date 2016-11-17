a1c4b65cae0bfee0a709fd15a846c768d2b52136
username4gh <jhon.doe1051@gmail.com>
2016-11-17 14:37:42 +0800

    fix: #29

--- sh/module/code_reading_utils/script/crutils_gen_file_list.bash
+++ sh/module/code_reading_utils/script/crutils_gen_file_list.bash
@@ -1,9 +1,6 @@
 #! /usr/bin/env bash
 
 echo "generating file list ..."
-while IFS= read -r item;
-do 
-    readlink -m "$item"; 
-done < <(find "$1" -name '*.aidl' -o -name '*.cc' -o -name '*.h' -o -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.py' | s -i -v 'test') | sort | uniq > './cscope.files'
+find "$1" -name '*.aidl' -o -name '*.cc' -o -name '*.h' -o -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.py' | s -i -v 'test' | _readlink | sort | uniq > './cscope.files'
 
 
--- sh/module/python/src/readline.bash
+++ sh/module/python/src/readline.bash
@@ -8,4 +8,6 @@ if [[ -f "/opt/local/bin/port" ]];then
     if [[ "$(port installed | s 'py35-readline')" == '' ]];then
         _sh_log "${BASH_SOURCE[0]}" "port install py35-readline"
     fi
+
+    export PYTHONSTARTUP=~/.pythonrc.py
 fi
