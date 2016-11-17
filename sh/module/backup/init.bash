10fa08d1c7de9636c452c85d7f6c075ad93a87eb
username4gh <jhon.doe1051@gmail.com>
2016-11-17 22:38:25 +0800

    refactor: rm some useless stuff

--- sh/module/backup/init.bash
+++ sh/module/backup/init.bash
@@ -2,4 +2,5 @@
 
 if [[ $(whoami) != root ]];then
     _load_sh_files $MY_SH_MODULE/backup src
+    export PATH="$MY_SH_MODULE/backup/script:$PATH"
 fi
--- sh/module/fzf/src/fzf.bash
+++ sh/module/fzf/src/fzf.bash
@@ -4,7 +4,3 @@
 if [[ ! -d "$HOME/.fzf" ]];then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 fi
-
-gdgsaf() {
-    git diff $(git status -s | s 'M' | awk '{print $2}' | fzf)
-}
