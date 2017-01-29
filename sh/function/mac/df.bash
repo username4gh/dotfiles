7d23660b76f0c964ed1ae4818255a488ebd8a5bd
username4gh <jhon.doe1051@gmail.com>
2017-01-29 23:35:17 +0800

    fix: sbs unix-linux compatibility issue

--- sh/function/mac/df.bash
+++ sh/function/mac/df.bash
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
-if [[ -f "/opt/local/libexec/gnubin/df" ]];then
+if [[  "$(uname)" == 'Darwin' ]];then
     df(){
         if [[ "$#" == 0 ]];then
-            (export LC_ALL="en_US.UTF-8"; /opt/local/libexec/gnubin/df)
+            (export LC_ALL="en_US.UTF-8"; gdf)
         else
-            (export LC_ALL="en_US.UTF-8"; /opt/local/libexec/gnubin/df "$@")
+            (export LC_ALL="en_US.UTF-8"; gdf "$@")
         fi
     }
 fi
