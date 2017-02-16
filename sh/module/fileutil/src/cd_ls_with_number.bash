ee9c98278b8e5c5e66dd3191d265be96a4b6e7ce
username4gh <jhon.doe1051@gmail.com>
2017-02-16 13:46:37 +0800

    feat: nlu & nud

--- sh/module/fileutil/src/cd_ls_with_number.bash
+++ sh/module/fileutil/src/cd_ls_with_number.bash
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-nls() {
+nlc() {
     if [[ "$#" == 0 ]];then
         local DIRS=( $(find . -mindepth 1 -maxdepth 1 -type d) )
 
@@ -22,5 +22,54 @@ ncd() {
         cd ${DIRS[$1]}
     else
         echo "plz enter a valid number, use nls to list all valid number"
+    fi
+}
+
+nlu() {
+    if [[ ! "$#" -gt 0 ]];then
+        local DIRS=()
+        local CURRENT_FOLDER="$(pwd)"
+        local MATCHING=true
+
+        while [ "$MATCHING" = true ]; do
+            if [[ "$CURRENT_FOLDER" =~ ^/$ ]]; then
+                DIRS+=( / )
+                MATCHING=false
+            else
+                DIRS+=( $CURRENT_FOLDER )
+                CURRENT_FOLDER=$(dirname "$CURRENT_FOLDER")
+            fi
+        done
+
+        local size=${#DIRS[@]}
+
+        for ((i = 0; i < size; i++));
+        do
+            echo "[$i]${DIRS[$i]}"
+        done 
+    else
+        echo "nlu does not support any option"
+    fi
+}
+
+nud() {
+    if [[ "$1" =~ [0-9]+ ]];then
+        local DIRS=()
+        local CURRENT_FOLDER="$(pwd)"
+        local MATCHING=true
+
+        while [ "$MATCHING" = true ]; do
+            if [[ "$CURRENT_FOLDER" =~ ^/$ ]]; then
+                DIRS+=( / )
+                MATCHING=false
+            else
+                DIRS+=( $CURRENT_FOLDER )
+                CURRENT_FOLDER=$(dirname "$CURRENT_FOLDER")
+            fi
+        done
+
+        cd ${DIRS[$1]}
+    else
+        echo "plz enter a valid number, use nlu to list all valid number"
     fi
 }
