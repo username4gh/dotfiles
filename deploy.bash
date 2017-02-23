bdee6b1c4f98663b8b3c41e917e2aa3148695df2
username4gh <jhon.doe1051@gmail.com>
2017-02-23 21:25:02 +0800

    feat: deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -1,20 +1,17 @@
 #! /usr/bin/env bash
 
-if [[ -f "$HOME/.bashrc" ]];then
-    mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
-fi
+files=('.bashrc' '.bash_profile' '.inputrc' '.vimrc')
+
+for((i=0; i<${#files[@]}; i++))
+do
+    rm -v "$HOME/${files[$i]}"
+
+done
 
 ln -s "$HOME/.dotfiles/.bash_profile" "$HOME/.bashrc"
 
-if [[ -f "$HOME/.bash_profile" ]];then
-    mv "$HOME/.bash_profile" "$HOME/.bash_profile.bak"
-fi
-
 ln -s "$HOME/.dotfiles/.bash_profile" "$HOME/.bash_profile"
-
-if [[ -f "$HOME/.vimrc" ]];then
-    mv "$HOME/.vimrc" "$HOME/.vimrc.bak"
-fi
 
 ln -s "$HOME/.dotfiles/.vimrc.light" "$HOME/.vimrc"
 
+ln -s "$HOME/.dotfiles/.inputrc" "$HOME/.inputrc"
