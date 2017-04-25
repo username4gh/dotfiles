1d2f67b5cb0b3199cfda14c6d8311e95c6915edd
username4gh <jhon.doe1051@gmail.com>
2017-04-26 01:58:04 +0800

    feat: filtering tool

--- sh/module/fzf/init.bash
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-if [[ $(whoami) != root ]];then
-    export PATH="$MY_SH_MODULE/fzf/script:$PATH"
-    _load_sh_files $MY_SH_MODULE/fzf src
-fi
--- sh/module/fzf/src/fzf.bash
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-#Description wrapper for fzf
-if [[ ! -d "$HOME/.fzf" ]];then
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-fi
