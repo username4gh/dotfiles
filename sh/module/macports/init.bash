f0c554dc1167628d9247a177ea41368b78b0283a
username4gh <jhon.doe1051@gmail.com>
2017-06-06 19:06:30 +0800

    feat: MACPORTS_DIST_FILE for convenieness

--- sh/module/macports/init.bash
+++ sh/module/macports/init.bash
@@ -2,4 +2,5 @@
 
 if [[ $(whoami) != root ]];then
     export PATH="$PATH:$MY_SH_MODULE/macports/script"
+    _load_sh_files $MY_SH_MODULE/macports src
 fi
---
+++ sh/module/macports/src/macports.bash
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+export MACPORTS_DIST_FILE='/opt/local/var/macports/distfiles'
