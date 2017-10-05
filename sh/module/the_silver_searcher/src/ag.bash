6c9fb8b49c5eedf75440bd34798ae8d297327b75
username4gh <jhon.doe1051@gmail.com>
2017-10-06 03:21:04 +0800

    feat: completion for the ag

--- sh/module/the_silver_searcher/src/ag.bash
+++ sh/module/the_silver_searcher/src/ag.bash
@@ -6,4 +6,7 @@ fi
 
 export PATH="$MY_DOTFILES_RESOURCES/the_silver_searcher:$PATH"
 
-alias ag='ag -p "$MY_SH_MODULE/the_silver_searcher/script/.ignore" "$@"'
+alias ag='ag -p "$MY_SH_MODULE/the_silver_searcher/script/.ignore"'
+
+# completion
+source "$MY_DOTFILES_RESOURCES/the_silver_searcher/ag.bashcomp.sh"
