215652e763570d1f73553c060a76fffa61ba9189
username4gh <jhon.doe1051@gmail.com>
2017-10-06 02:59:33 +0800

    fix: rm pyenv

--- sh/runtime/init.bash
+++ sh/runtime/init.bash
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
 if [[ $(whoami) != root ]];then
-    _load_sh_files $MY_SH/runtime src
     export PATH="$MY_SH/runtime/script:$PATH"
+    _load_sh_files $MY_SH/runtime src
 fi
---
+++ sh/runtime/src/python.bash
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+export PATH="$(python_site_user_base)/bin:$PATH"
