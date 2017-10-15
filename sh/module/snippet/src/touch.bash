cc3861016a491f66de4d63ebe73bfd2e01425862
username4gh <jhon.doe1051@gmail.com>
2017-10-15 23:42:02 +0800

    fix: #47

--- sh/module/snippet/src/touch.bash
+++ sh/module/snippet/src/touch.bash
@@ -4,16 +4,22 @@ _touch() {
     if [[ "$#" == 1 ]];then
         local path=$(_file_path $1)
         local name=$(_file_name_noext $1)
-        local ext=$(_file_ext $1)
 
-        if [[ -f "$MY_SH_MODULE/snippet/snippets/snippet.$ext" ]];then
-            cp "$MY_SH_MODULE/snippet/snippets/snippet.$ext" "$path/$name.$ext"
+        if [[ "${1#*.}" == "$1" ]]; then
+            command touch "$path/$name"
+            return
         else
-            command touch "$path/$name.$ext"
-        fi
+            local ext=$(_file_ext $1)
 
-        if [[ "$ext" == 'java' ]];then
-            gsed -i "s/public\ class\ Main/public\ class $name/g" "$path/$name.$ext"
+            if [[ -f "$MY_SH_MODULE/snippet/snippets/snippet.$ext" ]];then
+                cp "$MY_SH_MODULE/snippet/snippets/snippet.$ext" "$path/$name.$ext"
+            else
+                command touch "$path/$name.$ext"
+            fi
+
+            if [[ "$ext" == 'java' ]];then
+                gsed -i "s/public\ class\ Main/public\ class $name/g" "$path/$name.$ext"
+            fi
         fi
 
         unset path
