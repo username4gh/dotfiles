d8e01a9ce7e66a82b43fd1bc5354692c793c4839
username4gh <jhon.doe1051@gmail.com>
2017-10-24 18:03:43 +0800

    fix: ditching fzy

--- sh/module/enhancd/src/enhancd.bash
+++ sh/module/enhancd/src/enhancd.bash
@@ -4,14 +4,15 @@ if [[ ! -d "$MY_DOTFILES_RESOURCES/enhancd" ]];then
     git clone https://github.com/b4b4r07/enhancd "$MY_DOTFILES_RESOURCES/enhancd"
 fi
 
-if [[ ! -d "$MY_DOTFILES_RESOURCES/fzy" ]];then
-    git clone https://github.com/jhawthorn/fzy "$MY_DOTFILES_RESOURCES/fzy"
-fi
+#if [[ ! -d "$MY_DOTFILES_RESOURCES/fzy" ]];then
+#    git clone https://github.com/jhawthorn/fzy "$MY_DOTFILES_RESOURCES/fzy"
+#fi
 
-export PATH="$MY_DOTFILES_RESOURCES/fzy:$PATH"
+#export PATH="$MY_DOTFILES_RESOURCES/fzy:$PATH"
 
-export ENHANCD_FILTER=fzy
+export ENHANCD_FILTER=percol
 export ENHANCD_COMMAND='z'
+export ENHANCD_USE_FUZZY_MATCH=0
 
 if [[ -f "$MY_DOTFILES_RESOURCES/enhancd/init.sh" ]];then
     source "$MY_DOTFILES_RESOURCES/enhancd/init.sh"
