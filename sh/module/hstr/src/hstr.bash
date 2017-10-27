2be8468e4b465fcaadd215a4b1d419aecadf0ee9
username4gh <jhon.doe1051@gmail.com>
2017-10-27 23:04:38 +0800

    feat: using pm to install hstr rather than deal with its complexity by self-maintained code

--- sh/module/hstr/src/hstr.bash
+++ sh/module/hstr/src/hstr.bash
@@ -1,12 +1,7 @@
 #! /usr/bin/env bash
 
-if [[ ! -d "$MY_DOTFILES_RESOURCES/hstr" ]];then
-    git clone https://github.com/dvorka/hstr "$MY_DOTFILES_RESOURCES/hstr"
+if ! _is_command_exist 'hh';then
+    echo 'plz install hstr using homebrew or macports or anyother package manger software'
+else
+    export HH_CONFIG=hicolor
 fi
-
-_hstr_init() {
-    (pushd "$MY_DOTFILES_RESOURCES/hstr/dist" && ./1-dist.sh && pushd .. && ./configure && make)
-}
-
-export HH_CONFIG=hicolor
-export PATH="$MY_DOTFILES_RESOURCES/hstr/src:$PATH"
