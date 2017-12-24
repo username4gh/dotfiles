b94f52a5706325056be5e1f09755cd5c4351727c
username4gh <jhon.doe1051@gmail.com>
2017-12-24 18:31:26 +0800

    fix: some wrongful warning

--- sh/module/exiftool/src/exiftool.bash
+++ sh/module/exiftool/src/exiftool.bash
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
-if ! _is_command_exist exiftool;then
-    echo "Missing exiftool : go check out http://www.sno.phy.queensu.ca/~phil/exiftool/"
-fi
+_exiftool_init() {
+    if ! _is_command_exist exiftool;then
+        echo "Missing exiftool : go check out http://www.sno.phy.queensu.ca/~phil/exiftool/"
+    fi
+}
--- sh/module/hstr/src/hstr.bash
+++ sh/module/hstr/src/hstr.bash
@@ -1,7 +1,12 @@
 #! /usr/bin/env bash
 
-if ! _is_command_exist 'hh';then
-    echo 'plz install hstr using homebrew or macports or anyother package manger software'
-else
+_hstr_init() {
+    if ! _is_command_exist 'hh';then
+        echo 'plz install hstr using homebrew or macports or anyother package manger software'
+    fi
+}    
+
+if _is_command_exist 'hh';then
     export HH_CONFIG=hicolor
 fi
+
