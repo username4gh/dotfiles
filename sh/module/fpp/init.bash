a9aa55d30c7565a7c295a241149de7e22929ff4e
username4gh <jhon.doe1051@gmail.com>
2018-09-14 21:07:07 +0800

    feat: no more fpp due to its poor performance

--- sh/module/fpp/init.bash
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-if [[ $(whoami) != root ]];then
-    export PATH="$MY_SH_MODULE/fpp/script:$PATH"
-    _load_sh_files $MY_SH_MODULE/fpp src
-fi
--- sh/module/fpp/src/fpp.bash
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-_fpp_init() {
-    if [[ ! -d "$MY_DOTFILES_RESOURCES/PathPicker" ]];then
-        git clone 'https://github.com/facebook/PathPicker' "$MY_DOTFILES_RESOURCES/PathPicker"
-    fi
-}
