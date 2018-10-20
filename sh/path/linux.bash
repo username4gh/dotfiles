b0dd864bea809b0d3a21b0ed973cb0ad61f93357
username4gh <jhon.doe1051@gmail.com>
2018-10-20 12:22:52 +0800

    fix: bash completion setup under termux

--- sh/path/linux.bash
+++ sh/path/linux.bash
@@ -1,11 +1,21 @@
 #! /usr/bin/env bash
 
 if _is_linux;then
-    if ! shopt -oq posix; then
-        if [[ -f /usr/share/bash-completion/bash_completion ]]; then
-            . /usr/share/bash-completion/bash_completion
-        elif [[ -f /etc/bash_completion ]]; then
-            . /etc/bash_completion
+    if _is_termux;then    
+        if ! shopt -oq posix; then        
+            if [[ -f /data/data/com.termux/files/usr/share/bash-completion/bash_completion ]]; then            
+                . /data/data/com.termux/files/usr/share/bash-completion/bash_completion        
+            elif [[ -f /data/data/com.termux/files/etc/bash_completion ]]; then            
+                . /etc/bash_completion        
+            fi    
+        fi
+    else
+        if ! shopt -oq posix; then
+            if [[ -f /usr/share/bash-completion/bash_completion ]]; then
+                . /usr/share/bash-completion/bash_completion
+            elif [[ -f /etc/bash_completion ]]; then
+                . /etc/bash_completion
+            fi
         fi
     fi
 fi
