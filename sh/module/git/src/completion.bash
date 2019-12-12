4df12b0ae44b0b1d5d48789a066929227ca89162
username4gh <jhon.doe1051@gmail.com>
2019-12-12 14:36:51 +0800

    feat: completion for git

---
+++ sh/module/git/src/completion.bash
@@ -0,0 +1,23 @@
+_annotation_completion_write git clone
+_annotation_completion_write git init
+_annotation_completion_write git add
+_annotation_completion_write git mv
+_annotation_completion_write git reset
+_annotation_completion_write git rm
+_annotation_completion_write git bisect
+_annotation_completion_write git grep
+_annotation_completion_write git log
+_annotation_completion_write git show
+_annotation_completion_write git status
+_annotation_completion_write git branch
+_annotation_completion_write git checkout
+_annotation_completion_write git commit
+_annotation_completion_write git diff
+_annotation_completion_write git merge
+_annotation_completion_write git rebase
+_annotation_completion_write git tag
+_annotation_completion_write git fetch
+_annotation_completion_write git pull
+_annotation_completion_write git push
+
+_completion_setup git
